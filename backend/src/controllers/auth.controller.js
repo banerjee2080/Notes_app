642de3a3a1037d74dcda75cf1aa0dd9dcf74a22b
Amitabha Banerjee <banerjee2110@gmail.com>
2026-07-07 10:08:37 +0530

    logout fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -81,7 +81,12 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", "", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
     res.status(200).json({ message: "Logout Successful" });
   } catch (error) {
     console.log("Error in the logout: ", error);
