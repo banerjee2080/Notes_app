bce62398cd4de7bb1395022e015be650bd5945b7
Amitabha Banerjee <banerjee2110@gmail.com>
2026-07-07 10:13:47 +0530

    logout fix

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.totg7s7vng"
+    "revision": "0.l47ht3sadtc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
