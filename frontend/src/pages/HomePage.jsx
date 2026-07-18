7f77ca09a7dafd5aaec77d36c25ce10902a0b8e0
Amitabha Banerjee <banerjee2110@gmail.com>
2026-07-18 11:05:45 +0530

    seatch functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useLocation } from "react-router";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
